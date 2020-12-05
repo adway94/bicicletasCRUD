ef61855c2f2759f2a99e75233273c62d35a8833d
adway94 <alejandromachulsky_1o@hotmail.com>
2020-12-04 21:56:29 -0300

    se agrego la funcion crear, aun no funciona

--- controler/bicicleta.js
+++ controler/bicicleta.js
@@ -4,3 +4,14 @@ exports.bicicleta_list = function(req, res){
     res.render('bicicletas/index', {bicis: Bicicleta.allBicis})
 }
 
+exports.bicicleta_create_get = function(req, res){
+    res.render('bicicletas/create');
+}
+
+exports.bicicleta_create_post = function(req, res){
+    let bici = new Bicicleta(req.body.id, req.body.color, req.body.model);
+    bici.ubicacion = [req.body.lat, req.body.lng]
+    Bicicleta.add(bici)
+
+    res.redirect('/bicicletas')
+}
--- routes/bicicletas.js
+++ routes/bicicletas.js
@@ -3,5 +3,7 @@ const router = express.Router()
 const bicicletaControler = require('../controler/bicicleta')
 
 router.get('/', bicicletaControler.bicicleta_list);
+router.get('/create', bicicletaControler.bicicleta_create_get);
+router.post('/create', bicicletaControler.bicicleta_create_post);
 
 module.exports = router;
